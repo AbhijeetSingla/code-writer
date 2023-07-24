a8aaa1e197a62568d36cd0af188ec879932bd733
Abhijeet Singla <95906204+AbhijeetSingla@users.noreply.github.com>
2023-07-24 11:28:51 +0530

    refactor

--- src/index.js
+++ src/index.js
@@ -1,39 +1,46 @@
 import { format } from 'prettier'
 import { createWriteStream } from 'fs'
-import { generate_get_code, generate_scan_code } from './parameters.js'
+import { generate_delete_code, generate_get_code, generate_put_code, generate_scan_code } from './parameters.js'
 
-import structure from './sample_api_basic.json' assert { type: "json" };
+import structure from '../sample_api_basic.json' assert { type: "json" };
 
 const file_strings = {}
-const base_location = './generated_code_output/'
+const base_location = '../generated_code_output/'
 
 for (let index = 0; index < structure.length; index++) {
     const { access_type, file_name = 'generated_code', ...rest } = structure[index];
-    if(!file_strings.hasOwnProperty(file_name)) {
+    if (!file_strings.hasOwnProperty(file_name)) {
         file_strings[file_name] = ''
     }
     switch (access_type) {
         case 'scan':
             file_strings[file_name] += generate_scan_code(rest)
             break;
-    
+
         case 'get':
             file_strings[file_name] += generate_get_code(rest)
             break;
-    
+
+        case 'delete':
+            file_strings[file_name] += generate_delete_code(rest)
+            break;
+
+        case 'put':
+            file_strings[file_name] += generate_put_code(rest)
+            break;
+
         default:
             break;
     }
-    
 }
 
 for (const key in file_strings) {
-    console.log('why??: ', file_strings);
     format(file_strings[key], {
         parser: 'meriyah'
     }).then(data => {
         createWriteStream(`${base_location}${key}.js`).write(data)
     })
+    // createWriteStream(`${base_location}${key}.js`).write(file_strings[key])
 }
 
 console.log('complete')
--- src/parameters.js
+++ src/parameters.js
@@ -1,4 +1,4 @@
-import { generateGetParams, generateScanParams } from 'simple-dynamo-utils'
+import { generateDeleteParams, generateGetParams, generateScanParams } from 'simple-dynamo-utils'
 
 function generate_scan_code(parameters, file_string = ''){
     const { table_name, index_name = '', attributes_to_get, workers = 0, limit = 0, function_access_name } = parameters
@@ -31,11 +31,41 @@ function generate_get_code(parameters, file_string = ''){
             attribute: sort_key,
             value: 'sk_value_replace'
         }} : {}), 
-        ...(attributes_to_get && attributes_to_get === 'all' ? {projection: attributes_to_get.split(' ').filter(attr => attr).map(attr => attr.trim())} : {})
+        ...(attributes_to_get && attributes_to_get !== 'all' ? {projection: attributes_to_get.split(' ').filter(attr => attr).map(attr => attr.trim())} : {})
     }), null, 10).replace(`"pk_value_replace"`, `pk_value`).replace(`"sk_value_replace"`, `sk_value`)
     let function_starter_string = sort_key ? `async function ${function_access_name}(pk_value, sk_value) {` : `async function ${function_access_name}(pk_value) {`
     file_string += `${function_starter_string} \n return await get_item_function(${params_json}) \n }\n`
     return file_string
 }
 
-export { generate_scan_code, generate_get_code }
+function generate_delete_code(parameters, file_string = ''){
+    const { table_name, attributes_to_get, function_access_name, partition_key, sort_key = '' } = parameters
+    const params_json = JSON.stringify(generateDeleteParams({
+        table: table_name, 
+        partition: {
+            attribute: partition_key,
+            value: 'pk_value_replace'
+        }, 
+        ...(sort_key ? {sort: {
+            attribute: sort_key,
+            value: 'sk_value_replace'
+        }} : {}),
+        ...(attributes_to_get && attributes_to_get === 'all' ? { returnItem: true } : {})
+    }), null, 10).replace(`"pk_value_replace"`, `pk_value`).replace(`"sk_value_replace"`, `sk_value`)
+    let function_starter_string = sort_key ? `async function ${function_access_name}(pk_value, sk_value) {` : `async function ${function_access_name}(pk_value) {`
+    file_string += `${function_starter_string} \n return await delete_item_function(${params_json}) \n }\n`
+    return file_string
+}
+
+function generate_put_code(parameters, file_string = ''){
+    const { table_name, attributes_to_get = false, function_access_name } = parameters
+    const params_json = JSON.stringify({ 
+        TableName: table_name, 
+        Item: 'item_var_replace', 
+        ...(attributes_to_get && attributes_to_get === 'all' ? { ReturnValues: 'ALL_OLD' } : {})
+    }, null, 10).replace('"item_var_replace"', 'item')
+    file_string += `async function ${function_access_name}(item) {\n return await put_item_function( \n${params_json})\n}`
+    return file_string
+}
+
+export { generate_scan_code, generate_get_code, generate_delete_code, generate_put_code }
